3c7b284924a01fe372e259ef53c78af0a9622e55
Stepan <Steven.nnov@gmail.com>
2021-12-11 23:30:30 +0300

    HomeWork 3 files added

---
+++ HW3/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int n; // n - checked number
+    int a = 1;// a - answer, 1-integer 0-not integer
+    printf("Input integer number:");
+    scanf("%d", &n);
+    if (n > 1) {
+        if (n == 2 || n == 3){}
+        else {
+            for (int d = 2;d <= sqrtf(n);d++){
+                if(n % d == 0){
+                    a = 0;
+                    break;
+                }
+            }
+        }
+    }
+    else
+        a = 0;
+    (a == 1)? printf("%d is a PRIME number",n):printf("%d is NOT a prime number",n);
+    return 0;
+}
